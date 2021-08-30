6555769d84b22060e608ece232f6b26ca6b1eade
qawatake <greenmackey@gmail.com>
2021-08-30 12:10:00 +0900

    単項プラス・マイナスに対応

--- test.sh
+++ test.sh
@@ -23,6 +23,9 @@ assert 41 " 12 + 34 - 5 "
 assert 47 '5+6*7'
 assert 15 '5*(9-6)'
 assert 4 '(3+5)/2'
-
+assert 0 '-5+5'
+assert 10 '-10+20'
+assert 0 '-(3+5) + 8'
+assert 1 '-3*+5 + 16'
 
 echo OK
