aaed72126b44d1c8f959b48d79322d36df79c4ed
qawatake <greenmackey@gmail.com>
2021-08-30 16:18:04 +0900

    ファイルの分割

---
+++ 9cc.h
@@ -0,0 +1,94 @@
+#ifndef _INCLUDE_9CC_H_
+#define _INCLUDE_9CC_H_
+#include <stdbool.h>
+
+// トークンの種類
+typedef enum
+{
+  TK_RESERVED, // 記号
+  TK_NUM,      // 整数トークン
+  TK_EOF,      // 入力の終わりを表すトークン
+} TokenKind;
+
+// トークン型
+typedef struct Token Token; // Token_tag Token
+struct Token
+{
+  TokenKind kind; // トークンの型
+  Token *next;    // 次の入力のトークン
+  int val;        // kind が TK_NUM の場合, その数値
+  char *str;      // トークン文字列
+  int len;        // トークンの長さ 整数, EOFの場合は0
+};
+
+// エラー箇所をを報告する
+void error_at(char *loc, char *fmt, ...);
+
+// 次のトークンが期待している記号のときには, トークンを1つ進めて真を返す
+// それ以外の場合には偽を返す
+bool consume(char *op);
+
+// 次のトークンが期待している記号のときには, トークンを1つ進める
+// それ以外の場合にはエラーを報告する
+void expect(char *op);
+
+// 次のトークンが数値の場合, トークンを1つ読み進めてその数値を返す
+// それ以外の場合にはエラーを報告する
+int expect_number();
+
+bool at_eof();
+
+// 新しいトークンを作成して cur に繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str, int len);
+
+// Multi-letter punctuator か判定
+bool startswith(char *p, char *q);
+
+// 入力文字列 p をトークナイズしてそれを返す
+Token *tokenize(char *p);
+
+// 抽象構文木のノードの種類
+typedef enum
+{
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_EQ,  // ==
+  ND_NE,  // !=
+  ND_L,   // <
+  ND_LE,  // <=
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node; // Node_tag Node
+
+// 抽象構文木のノードの型
+struct Node
+{
+  NodeKind kind; // ノードの型
+  Node *lhs;     // 左辺
+  Node *rhs;     // 右辺
+  int val;       // kind が ND_NUM の場合のみ使う
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+
+void gen(Node *node);
+
+// 現在注目しているトークン
+extern Token *token;
+
+// 入力プログラム
+extern char *user_input;
+
+
+#endif
---
+++ parse.c
@@ -0,0 +1,262 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include "9cc.h"
+
+
+// エラー箇所をを報告する
+void error_at(char *loc, char *fmt, ...)
+{
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " ");
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// 次のトークンが期待している記号のときには, トークンを1つ進めて真を返す
+// それ以外の場合には偽を返す
+bool consume(char *op)
+{
+  if (token->kind != TK_RESERVED || token->len != strlen(op) || memcmp(token->str, op, token->len))
+    return false;
+  token = token->next;
+  return true;
+}
+
+// 次のトークンが期待している記号のときには, トークンを1つ進める
+// それ以外の場合にはエラーを報告する
+void expect(char *op)
+{
+  if (token->kind != TK_RESERVED || token->len != strlen(op) || memcmp(token->str, op, token->len))
+    error_at(token->str, "'%c'ではありません", op);
+  token = token->next;
+}
+
+// 次のトークンが数値の場合, トークンを1つ読み進めてその数値を返す
+// それ以外の場合にはエラーを報告する
+int expect_number()
+{
+  if (token->kind != TK_NUM)
+    error_at(token->str, "数ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof()
+{
+  return token->kind == TK_EOF;
+}
+
+// 新しいトークンを作成して cur に繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str, int len)
+{
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  tok->len = len;
+  cur->next = tok;
+  return tok;
+}
+
+bool startswith(char *p, char *q)
+{
+  return memcmp(p, q, strlen(q)) == 0;
+}
+
+// 入力文字列 p をトークナイズしてそれを返す
+Token *tokenize(char *p)
+{
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p)
+  {
+    // 空白文字をスキップ
+    if (isspace(*p))
+    {
+      p++;
+      continue;
+    }
+
+    // Multi-letter punctuator
+    if (startswith(p, "==") || startswith(p, "!=") || startswith(p, ">=") || startswith(p, "<="))
+    {
+      cur = new_token(TK_RESERVED, cur, p, 2);
+      p += 2;
+      continue;
+    }
+
+    // Single-letter punctuator
+    if (strchr("+-*/()<>", *p))
+    {
+      cur = new_token(TK_RESERVED, cur, p++, 1);
+      continue;
+    }
+
+    if (isdigit(*p))
+    {
+      cur = new_token(TK_NUM, cur, p, 0);
+      char *q = p;
+      cur->val = strtol(p, &p, 10);
+      cur->len = p - q;
+      continue;
+    }
+
+    error_at(p, "トークナイズできません");
+  }
+
+  new_token(TK_EOF, cur, p, 0);
+  return head.next;
+}
+
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr()
+{
+  return equality();
+}
+
+Node *equality()
+{
+  Node *node = relational();
+
+  for (;;)
+  {
+    if (consume("=="))
+    {
+      node = new_node(ND_EQ, node, relational());
+    }
+    else if (consume("!="))
+    {
+      node = new_node(ND_NE, node, relational());
+    }
+    else
+    {
+      return node;
+    }
+  }
+}
+
+Node *relational()
+{
+  Node *node = add();
+
+  for (;;)
+  {
+    if (consume(">"))
+    {
+      node = new_node(ND_L, add(), node);
+    }
+    else if (consume("<"))
+    {
+      node = new_node(ND_L, node, add());
+    }
+    else if (consume(">="))
+    {
+      node = new_node(ND_LE, add(), node);
+    }
+    else if (consume("<="))
+    {
+      node = new_node(ND_LE, node, add());
+    }
+    else
+    {
+      return node;
+    }
+  }
+}
+
+Node *add()
+{
+  Node *node = mul();
+
+  for (;;)
+  {
+    if (consume("+"))
+    {
+      node = new_node(ND_ADD, node, mul());
+    }
+    else if (consume("-"))
+    {
+      node = new_node(ND_SUB, node, mul());
+    }
+    else
+    {
+      return node;
+    }
+  }
+}
+
+Node *mul()
+{
+  Node *node = unary();
+
+  for (;;)
+  {
+    if (consume("*"))
+    {
+      node = new_node(ND_MUL, node, unary());
+    }
+    else if (consume("/"))
+    {
+      node = new_node(ND_DIV, node, unary());
+    }
+    else
+    {
+      return node;
+    }
+  }
+}
+
+Node *unary()
+{
+  if (consume("-"))
+  {
+    return new_node(ND_SUB, new_node_num(0), primary());
+  }
+  if (consume("+"))
+  {
+    return primary();
+  }
+  return primary();
+}
+
+Node *primary()
+{
+  // 次のトークンが "(" なら, "(" expr ")" のはず
+  if (consume("("))
+  {
+    Node *node = expr();
+    expect(")");
+    return node;
+  }
+
+  // そうでなければ数値のはず
+  return new_node_num(expect_number());
+}
